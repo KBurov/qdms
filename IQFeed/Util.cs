35a6fbe4ca414770edf7849422a2e8a652c4b8b2
Konstantin Burov <burov.konstantin@gmail.com>
2016-06-20 17:40:34 +0300

    Add all classes for IQFeed project

---
+++ IQFeed/Util.cs
@@ -0,0 +1,22 @@
+﻿// Imported from https://sentineliqf.codeplex.com
+
+namespace QDMSServer.DataSources
+{
+    public static class Util
+    {
+        public static string ArrayToString(string[] arr, char delimeter)
+        {
+            string ret = "";
+            if (arr == null) {
+                return ret;
+            }
+            for (int i = 0; i < arr.Length; i++) {
+                if (i > 0) {
+                    ret += delimeter;
+                }
+                ret += arr[i];
+            }
+            return ret;
+        }
+    }
+}
