252bbc3e6d67556f0dfb19e000c1f39aa98f8e79
Konstantin Burov <burov.konstantin@gmail.com>
2016-07-12 08:10:54 +0300

    Add RealTimeDataServer to the tests setup

--- QDMSTest/QDMSServer/HistoricalDataServerTest.cs
+++ QDMSTest/QDMSServer/HistoricalDataServerTest.cs
@@ -7,9 +7,13 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+
 using Moq;
+
 using NUnit.Framework;
+
 using QDMS;
+
 using QDMSServer;
 
 namespace QDMSTest
@@ -17,16 +21,22 @@ namespace QDMSTest
     [TestFixture]
     public class HistoricalDataServerTest
     {
+        private RealTimeDataServer _rtServer;
         private HistoricalDataServer _hdServer;
         private QDMSClient.QDMSClient _client;
-        private Mock<IHistoricalDataBroker> _brokerMock;
+        private Mock<IRealTimeDataBroker> _realTimeDataBrokerMock;
+        private Mock<IHistoricalDataBroker> _historicalDataBrokerMock;
 
         [SetUp]
         public void SetUp()
         {
-            _brokerMock = new Mock<IHistoricalDataBroker>();
-            
-            _hdServer = new HistoricalDataServer(5557, _brokerMock.Object);
+            _realTimeDataBrokerMock = new Mock<IRealTimeDataBroker>();
+            _historicalDataBrokerMock = new Mock<IHistoricalDataBroker>();
+            // Also need the real time server to keep the "heartbeat" going
+            _rtServer = new RealTimeDataServer(5555, 5554, _realTimeDataBrokerMock.Object);
+            _rtServer.StartServer();
+
+            _hdServer = new HistoricalDataServer(5557, _historicalDataBrokerMock.Object);
             _hdServer.StartServer();
 
             _client = new QDMSClient.QDMSClient("testingclient", "127.0.0.1", 5554, 5555, 5556, 5557);
@@ -47,28 +57,29 @@ namespace QDMSTest
             {
                 ID = 1,
                 Symbol = "SPY",
-                Datasource = new Datasource { ID = 1, Name = "MockSource" }
+                Datasource = new Datasource {ID = 1, Name = "MockSource"},
+                Exchange = new Exchange
+                {
+                    ID = 1,
+                    Name = "Exchange",
+                    Timezone = "Eastern Standard Time"
+                }
             };
-
-            instrument.Exchange = new Exchange()
-            {
-                ID = 1,
-                Name = "Exchange",
-                Timezone = "Eastern Standard Time"
-            };
-
             var request = new HistoricalDataRequest(instrument, BarSize.OneDay, new DateTime(2012, 1, 1), new DateTime(2013, 1, 1));
 
             _client.RequestHistoricalData(request);
 
             Thread.Sleep(500);
 
-            _brokerMock.Verify(x => x.RequestHistoricalData(
-                It.Is<HistoricalDataRequest>(r =>
-                    r.Instrument.ID == 1 &&
-                    r.Frequency == BarSize.OneDay &&
-                    r.StartingDate == new DateTime(2012, 1, 1) &&
-                    r.EndingDate == new DateTime(2013, 1, 1))), Times.Once);
+            _historicalDataBrokerMock.Verify(
+                x => x.RequestHistoricalData(
+                    It.Is<HistoricalDataRequest>(
+                        r =>
+                            r.Instrument.ID == 1 &&
+                            r.Frequency == BarSize.OneDay &&
+                            r.StartingDate == new DateTime(2012, 1, 1) &&
+                            r.EndingDate == new DateTime(2013, 1, 1))),
+                Times.Once);
         }
 
         [Test]
@@ -91,26 +102,27 @@ namespace QDMSTest
             {
                 ID = 1,
                 Symbol = "SPY",
-                Datasource = new Datasource { ID = 1, Name = "MockSource" }
+                Datasource = new Datasource {ID = 1, Name = "MockSource"}
             };
-
             var data = new List<OHLCBar>
             {
-                new OHLCBar {Open = 1, High = 2, Low = 3, Close = 4, DT = new DateTime(2013, 1, 1) }
+                new OHLCBar {Open = 1, High = 2, Low = 3, Close = 4, DT = new DateTime(2013, 1, 1)}
             };
-
-            var req = new DataAdditionRequest(BarSize.OneDay, instrument, data, true);
+            var req = new DataAdditionRequest(BarSize.OneDay, instrument, data);
 
             _client.PushData(req);
 
             Thread.Sleep(50);
 
-            _brokerMock.Verify(x => x.AddData(
-                It.Is<DataAdditionRequest>(y =>
-                    y.Frequency == BarSize.OneDay &&
-                    y.Instrument.ID == 1 &&
-                    y.Data.Count == 1)
-                ), Times.Once);
+            _historicalDataBrokerMock.Verify(
+                x => x.AddData(
+                    It.Is<DataAdditionRequest>(
+                        y =>
+                            y.Frequency == BarSize.OneDay &&
+                            y.Instrument.ID == 1 &&
+                            y.Data.Count == 1)
+                    ),
+                Times.Once);
         }
 
         [Test]
@@ -120,43 +132,45 @@ namespace QDMSTest
             {
                 ID = 1,
                 Symbol = "SPY",
-                Datasource = new Datasource { ID = 1, Name = "MockSource" }
+                Datasource = new Datasource {ID = 1, Name = "MockSource"}
             };
 
             _client.GetLocallyAvailableDataInfo(instrument);
 
-            Thread.Sleep(50);
+            Thread.Sleep(1000);
 
-            _brokerMock.Verify(x => x.GetAvailableDataInfo(
-                It.Is<Instrument>(y =>
-                    y.ID == 1 &&
-                    y.Symbol == "SPY")
-                    ), Times.Once);
+            _historicalDataBrokerMock.Verify(
+                x => x.GetAvailableDataInfo(
+                    It.Is<Instrument>(
+                        y =>
+                            y.ID == 1 &&
+                            y.Symbol == "SPY")
+                    ),
+                Times.Once);
         }
 
         [Test]
         public void SendsErrorMessageWhenExceptionIsRaisedByBrokerOnHistoricalDataRequest()
         {
-            bool errorRaised = false;
+            var errorRaised = false;
+
             _client.Error += (sender, e) => errorRaised = true;
 
             var instrument = new Instrument
             {
                 ID = 1,
                 Symbol = "SPY",
-                Datasource = new Datasource { ID = 1, Name = "MockSource" }
+                Datasource = new Datasource {ID = 1, Name = "MockSource"},
+                Exchange = new Exchange
+                {
+                    ID = 1,
+                    Name = "Exchange",
+                    Timezone = "Eastern Standard Time"
+                }
             };
-
-            instrument.Exchange = new Exchange()
-            {
-                ID = 1,
-                Name = "Exchange",
-                Timezone = "Eastern Standard Time"
-            };
-
             var request = new HistoricalDataRequest(instrument, BarSize.OneDay, new DateTime(2012, 1, 1), new DateTime(2013, 1, 1));
 
-            _brokerMock.Setup(x => x.RequestHistoricalData(It.IsAny<HistoricalDataRequest>())).Throws(new Exception("error message"));   
+            _historicalDataBrokerMock.Setup(x => x.RequestHistoricalData(It.IsAny<HistoricalDataRequest>())).Throws(new Exception("error message"));
 
             _client.RequestHistoricalData(request);
 
